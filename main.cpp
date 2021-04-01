5f1bf4318a221b4ca5bc5a8534080976445a073a
MichaelRosado1 <mrosad4@illinois.edu>
2021-03-31 23:40:51 -0500

    main functionreads and outputs the text of the txt file when passed as an argument to the program

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
+int main(int argc, char ** argv) {
+	std::string line;
+	std::ifstream file (argv[1]);
 
-int main() {
-	std::cout<<"Hello, world!"<<std::endl;
+	if (file) {
+		while (std::getline (file, line)) {
+			std::cout<<line<<std::endl;
+		}
+	}
 }
