e887afb3e4f2f376a21a0e8a2324c3c133c12d84
MichaelRosado1 <mrosad4@illinois.edu>
2021-04-02 21:44:32 -0500

    created basic layout of the Nary tree class

--- suffixTree.h
+++ suffixTree.h
@@ -13,6 +13,43 @@ class NaryTree {
 			//every node will contain a map to child nodes
 			std::map<std::string, Node*> children;
 
+			//index vector will contain the occurances of the specific pattern
+			//in the text
+			std::vector<int> index; 
+
+			//Node constructor
+			//for a suffix tree we are going to want to store the 
+			Node(std::string s, int i) {
+				if (s.length() > 0) {
+					//recursive defenition of the node
+					children[s[0]] = new Node(s.substr(1, i));
+				} else {
+					index.push_back(i);
+				}
+			}
+
+			std::bool isLeaf() {
+				if (children.size() == 0) {
+					return true;
+				} else {
+					return false;
+				}
+			}
 
 		};
+
+		//NaryTree constructor
+		NaryTree();
+
+		//NaryTree destructor
+		~NaryTree();		
+
+		//@return the root of the tree
+		Node* getRoot() const; 
+
+		//@return the height of the tree
+		int getHeight() const;
+		
+	private:
+		Node* root;
 };
